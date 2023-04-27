1865bb5d1b94a978dde35e95a70706e8c99cfb96
Wint3rmute <mateusz.baczek1998@gmail.com>
2023-04-27 16:56:57 +0200

    add project switch which key to lvim

--- lvim/config.lua
+++ lvim/config.lua
@@ -23,6 +23,7 @@ lvim.leader                                                 = "space"
 lvim.keys.normal_mode["<S-l>"]                              = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"]                              = ":BufferLineCyclePrev<CR>"
 lvim.builtin.which_key.mappings["t"]                        = { "<cmd>TroubleToggle<cr>", "Tougle Trouble list" }
+lvim.builtin.which_key.mappings["P"]                        = { "<cmd>Telescope projects<cr>", "Switch project" }
 
 -- -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
