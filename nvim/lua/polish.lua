c30500103d4888aa10fed8c623e9506f4a291bf7
Mateusz Bączek <mateusz.baczek@creotech.pl>
2024-09-15 13:09:06 +0200

    Add neovide config

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -1,18 +1,9 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
--- Set up custom filetypes
-vim.filetype.add {
-  extension = {
-    foo = "fooscript",
-  },
-  filename = {
-    ["Foofile"] = "fooscript",
-  },
-  pattern = {
-    ["~/%.config/foo/.*"] = "fooscript",
-  },
-}
+if vim.g.neovide then
+  vim.o.guifont = "Source Code Pro:h12" -- text below applies for VimScript
+  vim.g.neovide_floating_blur_amount_x = 2.0
+  vim.g.neovide_floating_blur_amount_y = 2.0
+end
