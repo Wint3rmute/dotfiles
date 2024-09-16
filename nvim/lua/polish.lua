7afe1749bf988192d0c955c3ca628b8d422775cf
Wint3rmute <mateusz.baczek1998@gmail.com>
2024-09-16 19:05:58 +0200

    neovim config formatting

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -3,7 +3,7 @@
 -- fit in the normal config locations above can go here
 
 if vim.g.neovide then
-  vim.o.guifont = "Source Code Pro:h12" -- text below applies for VimScript
-  vim.g.neovide_floating_blur_amount_x = 2.0
-  vim.g.neovide_floating_blur_amount_y = 2.0
+	vim.o.guifont = "Source Code Pro:h12" -- text below applies for VimScript
+	vim.g.neovide_floating_blur_amount_x = 2.0
+	vim.g.neovide_floating_blur_amount_y = 2.0
 end
