9205ecc802018fbb84c78396beeefaff4b1b772d
Mateusz Bączek <mateusz.baczek@creotech.pl>
2025-04-02 12:37:15 +0200

    update to astronvim v5

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -5,8 +5,9 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
+  -- { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.angular" },
+  -- { import = "astrocommunity.pack.angular" },
 }
--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -1,9 +1,5 @@
--- This will run last in the setup process and is a good place to configure
--- things like custom filetypes. This just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
+if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
-if vim.g.neovide then
-	vim.o.guifont = "Source Code Pro:h12" -- text below applies for VimScript
-	vim.g.neovide_floating_blur_amount_x = 2.0
-	vim.g.neovide_floating_blur_amount_y = 2.0
-end
+-- This will run last in the setup process.
+-- This is just pure lua so anything that doesn't
+-- fit in the normal config locations above can go here
